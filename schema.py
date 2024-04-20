9e606678135168edbda90f2eadccde2be1c1221c
AsiaM425 <123774776+AsiaM425@users.noreply.github.com>
2024-04-20 17:55:49 -0400

    Create schema.py

---
+++ schema.py
@@ -0,0 +1,20 @@
+CREATE TABLE Students (
+    id INTEGER PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT
+);
+
+CREATE TABLE Quizzes (
+    id INTEGER PRIMARY KEY,
+    subject TEXT,
+    num_questions INTEGER,
+    quiz_date DATE
+);
+
+CREATE TABLE QuizResults (
+    student_id INTEGER,
+    quiz_id INTEGER,
+    score INTEGER,
+    FOREIGN KEY(student_id) REFERENCES Students(id),
+    FOREIGN KEY(quiz_id) REFERENCES Quizzes(id)
+);
