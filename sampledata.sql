6a7d6a203896af97ea325e0986c2e5e0e823621e
AsiaM425 <123774776+AsiaM425@users.noreply.github.com>
2024-04-20 17:56:48 -0400

    Create sampledata.sql

---
+++ sampledata.sql
@@ -0,0 +1,3 @@
+INSERT INTO Students (id, first_name, last_name) VALUES (1, 'John', 'Smith');
+INSERT INTO Quizzes (id, subject, num_questions, quiz_date) VALUES (1, 'Python Basics', 5, '2015-02-05');
+INSERT INTO QuizResults (student_id, quiz_id, score) VALUES (1, 1, 85);
